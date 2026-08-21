eada44142e6aecc14a0d3865a12d990134a9a066
Nabeela0904 <nabeelafathima45@gmail.com>
2026-08-21 15:11:33 +0530

    Update liveUrl for Shoaib & Zeenath invitation project

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -53,7 +53,7 @@ export const PORTFOLIO_PROJECTS: PortfolioProject[] = [
       company: 'Royal Digital Invitation'
     },
     imageAccent: 'from-amber-500/25 via-pink-500/20 to-purple-600/25',
-    liveUrl: 'https://suhelandnasreenweddinginvitation.netlify.app/'
+    liveUrl: 'https://shoaibandzeenathziyafatnama.netlify.app/invitation.html'
   },
   {
     id: 'yasatoz-ecommerce-marketplace',
